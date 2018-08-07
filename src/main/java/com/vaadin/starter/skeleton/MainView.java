b2ebc5af5c5d1ca9a6cb387c842b53e86b783fc6
Mikko <mikko@vaadin.com>
2018-08-07 11:09:32 +0300

    Added PWA support for starter.
    * Added PWA -annotation to enable pwa resources
    * Added icon matching starter-page icon
    * Added flow-server dependency to support PWA
      (Remove when proper version available)

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -4,12 +4,14 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
 
 /**
  * The main view contains a button and a template element.
  */
 @HtmlImport("styles/shared-styles.html")
 @Route("")
+@PWA(name = "Base Starter for Vaadin Flow", shortName = "Base Starter")
 public class MainView extends VerticalLayout {
 
     public MainView() {
