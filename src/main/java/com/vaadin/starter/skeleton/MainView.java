dd7eb458c9d412faff46e5d038dc61633fbf1b3a
Klaudeta Mertiku <klaudeta@vaadin.com>
2018-09-20 10:53:59 +0300

    Push websocket failing with Custom vaadin servlet. Problem: WARNING: More than one Servlet Mapping defined. WebSocket may not work org.apache.catalina.core.ApplicationServletRegistration@637edfe0, org.atmosphere.container.JSR356AsyncSupport <init> INFO: JSR 356 Mapping path /frontend In order to work mapping path should be /vaadin

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,19 +1,63 @@
 package com.vaadin.starter.skeleton;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServlet;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route
+@Push
 public class MainView extends VerticalLayout {
 
     public MainView() {
         Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+                event -> {
+					new Thread(() ->  {
+			        	try {
+							Thread.currentThread().sleep(2000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+			        	
+			        	getUI().ifPresent(ui -> {
+			        		ui.access(() -> {
+			        			Notification.show("Pushed!");
+			        		} );
+			        	});
+			        	
+			        	
+			        }).start();;
+				});
+  
+        
         add(button);
+        
+        
+       
+    }
+    
+//    @WebServlet(name="frontendServlet", urlPatterns= { "/frontend/*"}, asyncSupported=true)
+//    public static class FrontendServlet extends VaadinServlet{
+//    	
+//    }
+    
+    @WebServlet(name="customServlet", urlPatterns= { "/vaadin/*", "/frontend/*"}, asyncSupported=true)
+    public static class CustomVaadinServlet extends VaadinServlet{
+    	
+    	@Override
+    	public void init() throws ServletException {
+    		// TODO Auto-generated method stub
+    		super.init();
+    	}
     }
 }
+
